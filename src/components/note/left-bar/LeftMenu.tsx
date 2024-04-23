88139422ab605062c041e00b267344ee0b5cbde8
Koushik <programmer.koushik@gmail.com>
2024-04-23 22:38:20 +0530

    added Notes btn

---
+++ src/components/note/left-bar/LeftMenu.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import LeftMenuBtn from "./LeftMenuBtn";
+
+export default function LeftMenu() {
+  return (
+    <div className="ml-5 mt-5 w-56">
+      <LeftMenuBtn>Notes</LeftMenuBtn>
+    </div>
+  );
+}
