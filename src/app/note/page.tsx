e1d45ff498ab03c072fbbf9a7e246a4a64a9744b
Koushik <programmer.koushik@gmail.com>
2024-04-23 22:34:44 +0530

    Note page: added NoteDetails component

---
+++ src/app/note/page.tsx
@@ -0,0 +1,12 @@
+import NoteDetails from "@/components/note/NoteDetails";
+import React from "react";
+
+export default function Note() {
+  return (
+    <main>
+      <section>
+        <NoteDetails />
+      </section>
+    </main>
+  );
+}
