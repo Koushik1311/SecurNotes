2bfffea11938a6203b076b2da3632cece218cab6
Koushik <programmer.koushik@gmail.com>
2024-04-23 14:07:49 +0530

    init

---
+++ src/app/(root)/(home)/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center text-6xl">
+      Welcome to SecurNotes
+    </div>
+  );
+}
