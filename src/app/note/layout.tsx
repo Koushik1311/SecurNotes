da9afc96e8b291f43896bc10bb47ab5c9938303a
Koushik <programmer.koushik@gmail.com>
2024-04-23 22:36:07 +0530

    Note layout: added LeftMenu

---
+++ src/app/note/layout.tsx
@@ -0,0 +1,15 @@
+import LeftMenu from "@/components/note/left-bar/LeftMenu";
+
+export default function NotesLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="w-screen flex flex-row space-x-5">
+      <LeftMenu />
+      <div className="border-l border-slate-200" />
+      <div className="flex-1">{children}</div>
+    </div>
+  );
+}
