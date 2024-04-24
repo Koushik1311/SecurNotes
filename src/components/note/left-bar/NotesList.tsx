cbbee72d19679336a7681c47e1fce61a4ca670ff
Koushik <programmer.koushik@gmail.com>
2024-04-24 20:59:31 +0530

    Update NotesList to display items from Redux store with links to note details

--- src/components/note/left-bar/NotesList.tsx
+++ src/components/note/left-bar/NotesList.tsx
@@ -1,13 +1,28 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
+import { RootState } from "@/redux/store";
+import { Item } from "@/types/Item";
+import { useSelector } from "react-redux";
 
 export default function NotesList() {
+  const items: Item[] = useSelector(
+    (state: RootState) => state.itemsReducer.value
+  );
+
   return (
     // List of Notes
-    <div>
-      <div className="w-56 h-[30px] truncate pl-2 text-sm font-medium text-gray-500 rounded-md hover:bg-slate-200 transition-all flex flex-row items-center">
-        <Link href="#">How to get started with Next.js</Link>
-      </div>
-    </div>
+    <>
+      {items.map((item, index) => (
+        <div key={index} className="w-56">
+          <div className="h-[30px] truncate px-2 text-sm font-medium text-gray-500 rounded-md hover:bg-slate-200 transition-all flex flex-row items-center overflow-hidden">
+            <Link href={`/note/${item.id}`} className="truncate">
+              {item.title || "Untitled"}
+            </Link>
+          </div>
+        </div>
+      ))}
+    </>
   );
 }
