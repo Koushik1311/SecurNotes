909ceda86e98e1227eefc9054ab89a80723c0e84
Koushik <programmer.koushik@gmail.com>
2024-04-24 20:47:12 +0530

    individual note details

---
+++ src/app/note/[id]/page.tsx
@@ -0,0 +1,10 @@
+import NoteDetails from "@/components/note/NoteDetails";
+import React from "react";
+
+export default function SingleNote() {
+  return (
+    <div>
+      <NoteDetails />
+    </div>
+  );
+}
