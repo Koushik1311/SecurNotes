bfbc34b64713abc8463d3ba06c61b1964f708b91
Koushik <programmer.koushik@gmail.com>
2024-04-27 07:59:04 +0530

    added Sono font

--- src/app/note/layout.tsx
+++ src/app/note/layout.tsx
@@ -1,4 +1,7 @@
+import { Sono } from "next/font/google";
 import LeftMenu from "@/components/note/left-bar/LeftMenu";
+
+const sono = Sono({ subsets: ["latin"] });
 
 export default function NotesLayout({
   children,
@@ -9,7 +12,7 @@ export default function NotesLayout({
     <div className="flex flex-row">
       <LeftMenu />
       <div className="border-l border-slate-200 ml-5" />
-      <div className="flex-1">{children}</div>
+      <div className={`${sono.className} flex-1`}>{children}</div>
     </div>
   );
 }
