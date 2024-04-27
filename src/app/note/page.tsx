e765d06d4aa8be27e1535ca4a30a7ce3fd235d8d
Koushik <programmer.koushik@gmail.com>
2024-04-27 08:00:19 +0530

    Update Note component page and integrate Banner and AddItemButton components

--- src/app/note/page.tsx
+++ src/app/note/page.tsx
@@ -1,13 +1,38 @@
-import NoteDetails from "@/components/note/NoteDetails";
+"use client";
+
+import AddItemButton from "@/components/global/AddItemButton";
+import Banner from "@/components/note/home/Banner";
+import Image from "next/image";
 import React from "react";
 
 export default function Note() {
   return (
     <main>
-      {/* <section>
-        <NoteDetails />
-      </section> */}
-      <h1>Welcome</h1>
+      <section>
+        <Banner />
+      </section>
+      <section className="-mt-12 ml-24">
+        <Image
+          src="hand.svg"
+          width={80}
+          height={80}
+          quality={100}
+          alt="hand"
+          priority
+        />
+
+        <h1 className="text-5xl font-bold text-gray-700 mt-10">
+          Welcome Back!
+        </h1>
+      </section>
+      <section className="ml-24 mt-10 flex flex-row">
+        <div>
+          <AddItemButton className="text-lg font-semibold text-gray-400 hover:text-gray-800 transition-colors">
+            Add Item
+          </AddItemButton>
+        </div>
+        <div></div>
+      </section>
     </main>
   );
 }
